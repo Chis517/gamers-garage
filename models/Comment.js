f540dd62f02b5b313398294b26b88b92a538efe1
Chice <cko517@hotmail.com>
2022-03-01 20:25:10 -0500

    Models and dateFormat util

---
+++ models/Comment.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const commentSchema = new Schema(
+  {
+    commentText: {
+      type: String,
+      required: 'You need to leave a comment!',
+      minlength: 1,
+      maxlength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: timestamp => dateFormat(timestamp)
+    },
+    username: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+commentSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+});
+
+const Comment = model('Comment', commentSchema);
+
+module.exports = Comment;
