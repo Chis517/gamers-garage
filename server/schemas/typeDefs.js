465a09e277da789521fe80b7ad0a9bc003a147c9
Jess Varghese <jessvarghese@gmail.com>
2022-03-02 21:09:15 -0500

    updated typeDef code

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,52 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+type Query {
+  me: User
+}
+
+  type User {
+    _id: ID!
+    username: String
+    email: String
+    savedGames: [Game]
+  
+  }
+
+  type Game {
+    gameId: ID!
+    released: String
+    description: String
+    background_image: String
+    website: String
+    rating: Int
+  }
+
+  input savedGamesInput {
+    released: String
+    description: String
+    background_image: String
+    website: String
+    rating: Int
+}
+
+
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveGame(body: savedGameInput): User
+    removeGame(gameId: ID!): User
+    
+  }
+
+  
+`;
+
+module.exports = typeDefs;
