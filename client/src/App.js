b5eba1066c60533fa0558af6fe723e5fd318fa19
Brandon Conte <bconte950@gmail.com>
2022-03-06 17:11:45 -0500

    Navbar and logout page added.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import RegisterUser from '../src/pages/Signup';
+import LoginUser from '../src/pages/Login';
+import Logout from '../src/pages/Logout';
+import Navbar from './components/Navbar';
+import Home from './components/pages/Home';
+import Genre from './components/pages/Genre';
+import Platforms from './components/pages/Platforms';
+import Games from './components/pages/Games';
+import Footer from './components/pages/Footer';
+import PlatformPage from './components/PlatformPage';
+import GameDetails from './components/pages/GameDetails';
+import Profile from './components/pages/Profile';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+        <Router>
+          <div>
+            <Navbar />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/genre" component={Genre} />
+            <Route exact path="/platforms" component={Platforms} />
+            <Route exact path="/games" component={Games} />
+            <Route path="/games/:pageid" component={Games} />
+            <Route path="/platformpage/:platform" component={PlatformPage} />
+            <Route path="/game/:name/:id" component={GameDetails} />
+            <Route path="/register" component={RegisterUser} />
+            <Route path="/login" component={LoginUser} />
+            <Route path="/logout" component={Logout} />
+            <Route path="/profile" component={Profile} />
+            <Footer />
+          </div>
+        </Router>
+    );
+  }
 }
-
 export default App;
